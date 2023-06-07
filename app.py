683dfc37c411774162f11b31e4b7749f8b9204ac
mich2k <miky3848@gmail.com>
2023-06-07 18:16:41 +0200

    in retrieval in pipeline

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from gazedetection.gaze_detection.main import GazeDetection
+from face_retrieval.src.face_retrieval.retrieval import Retrieval
 import cv2
 
 
 gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.3,0,0,0.1))
+retrieval = Retrieval("def_blacklist.pt")
 
 vid = cv2.VideoCapture(0)
 while(True):
@@ -10,7 +12,9 @@ while(True):
 
     return_frame, is_gaze_facing = gaze_detection.detect(frame)
     
-    print(f"GAZE IS FACING CAMERA:     {is_gaze_facing}       ", end='\r')
+    ret = retrieval.evaluateFrame(return_frame)
+    
+    print(f"GAZE IS FACING CAMERA:     {is_gaze_facing}       \nIs blacklisted:     {'Found' if ret else 'Not Found'}", end='\r')
 
     cv2.imshow('frame',  return_frame)
                 
