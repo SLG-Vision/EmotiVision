aa0ba2f0283789393c884bb24071a4d6cde1c98d
mich2k <miky3848@gmail.com>
2023-06-09 11:57:01 +0200

    avg distance

--- pipeline/app.py
+++ pipeline/app.py
@@ -4,12 +4,13 @@ from fer_classification.net.FERNet import FERNet
 from fer_classification.net.utils import transform
 import cv2
 import torch
+import time
 
 net = FERNet()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 net.load_state_dict(torch.load('pipeline/student_distilled.t7', map_location=device))
 gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.3,0,0,0.1))
-retrieval = Retrieval("def_blacklist.pt", threshold=0.6)
+retrieval = Retrieval("def_blacklist.pt", threshold=0.85, debug=True, debugAverage=True)
 
 label = {0:'angry', 1:'disgust', 2:'fear', 3:'happy',  4:'sad', 5:'surprise', 6:'neutral'}
 
@@ -24,10 +25,10 @@ while(True):
     ret = retrieval.evaluateFrame(return_frame)
     
     print(f"Gaze is facing:     {is_gaze_facing} \n Is blacklisted:     {'Found' if ret else 'Not Found'} \n Emotion Detected: {label[int(torch.argmax(pred).item())]}")
-
     cv2.imshow('frame',  return_frame)
                 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    time.sleep(0.5)
 
 vid.release()
