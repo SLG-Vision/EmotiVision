2a1fdfe5a203d77b467571ab6d7554cce08560d2
mich2k <miky3848@gmail.com>
2023-06-09 16:12:53 +0200

    formatting

--- pipeline/app.py
+++ pipeline/app.py
@@ -9,11 +9,11 @@ net = FERNet()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 net.load_state_dict(torch.load('pipeline/student_distilled.t7', map_location=device))
 gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.4,0.3,0.1,0.3))
-retrieval = Retrieval("def_blacklist.pt", threshold=0.90, debug=True, debugAverage=True, usingAverage=True, toVisualize=True, usingMtcnn=False)
+retrieval = Retrieval("def_blacklist.pt", threshold=0.90, debug=False, debugAverage=True, usingAverage=True, toVisualize=True, usingMtcnn=False)
 
 fernet_label = {0:'angry', 1:'disgust', 2:'fear', 3:'happy',  4:'sad', 5:'surprise', 6:'neutral'}
 
-retrieval_label = {1:'Detected and identified', 2:'Detected and not recognized', 3: 'Not detected', 4: 'Not available yet',}
+retrieval_label = {1:'Detected and identified', 2:'Detected and not recognized', 3: 'Not available yet',}
 
 vid = cv2.VideoCapture(0)
 while(True):
@@ -30,7 +30,7 @@ while(True):
     pred = net(input_tensor)
 
     
-    print(f"Gaze is facing:     {is_gaze_facing} \n Is blacklisted status:     {retrieval_label[ret]} \n    Emotion Detected: {fernet_label[int(torch.argmax(pred).item())]}")
+    print(f"\nGaze is facing:\t{is_gaze_facing}\t\n\tRetrieval status: {retrieval_label[ret]}\t\n\tEmotion Detected: {fernet_label[int(torch.argmax(pred).item())]}")
     cv2.imshow('frame',  return_frame)
                 
     if cv2.waitKey(1) & 0xFF == ord('q'):
