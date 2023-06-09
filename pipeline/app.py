a0721f4eec5af03e4b4963b6c672f1b28a5a5ddd
mich2k <miky3848@gmail.com>
2023-06-09 12:03:17 +0200

    adj margins

--- pipeline/app.py
+++ pipeline/app.py
@@ -9,7 +9,7 @@ import time
 net = FERNet()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 net.load_state_dict(torch.load('pipeline/student_distilled.t7', map_location=device))
-gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.3,0,0,0.1))
+gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.4,0.3,0.1,0.3))
 retrieval = Retrieval("def_blacklist.pt", threshold=0.85, debug=True, debugAverage=True)
 
 label = {0:'angry', 1:'disgust', 2:'fear', 3:'happy',  4:'sad', 5:'surprise', 6:'neutral'}
