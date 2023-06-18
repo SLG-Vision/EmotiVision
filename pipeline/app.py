75b22706331126e3e501f015fb77f28f9df6cfba
Theviki20110 <vincenzolapadula23@gmail.com>
2023-06-18 15:08:06 +0200

    refactor

--- pipeline/app.py
+++ pipeline/app.py
@@ -1,20 +1,15 @@
 from face_retrieval.src.retrieval import Retrieval
 from gaze_detection.src.main import GazeDetection
-from fer_classification.net.FERNet import FERNet
-from fer_classification.net.utils import transform
+from fer_classification.net.FERModule import FER
 import cv2
-import torch
 
-net = FERNet()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-net.load_state_dict(torch.load('pipeline/student_distilled.t7', map_location=device))
+net = FER()
+net.load()
+
 gaze_detection = GazeDetection(predictor_path="shape_predictor_68_face_landmarks.dat", video=False, print_on_serial=False, crop_frame_paddings=(0.0,0.0,0.0,0.0))
 
 # performing hyperparameters: {thr: 0:18, usingAverage=True}
 retrieval = Retrieval("def_blacklist.pt", threshold=0.18, debug=True, distanceMetric='cosine', usingAverage=True, usingMedian=False, usingMax=False, toVisualize=True, usingMtcnn=False)
-
-fernet_label = {0:'angry', 1:'disgust', 2:'fear', 3:'happy',  4:'sad', 5:'surprise', 6:'neutral'}
-
 
 vid = cv2.VideoCapture(0)
 while(True):
@@ -35,8 +30,6 @@ while(True):
     
     #retrieval.setUsingMtcnn(True)
     #code, retrieval_return_string = retrieval.evalFrameTextual(frame)
-
-    
     
     if(retrieval.hasMtcnnFailed()):
         continue
@@ -44,12 +37,7 @@ while(True):
     if(retrieval.isPersonBlacklisted()):
         pass    # no tracking condition
     
-    input_tensor = transform(return_frame)
-    input_tensor = input_tensor.unsqueeze(0)
-    pred = net(input_tensor)
-
-    
-    print(f"\nGaze is facing:\t{is_gaze_facing}\t\n\tRetrieval status: {retrieval_return_string}\t\n\tEmotion Detected: {fernet_label[int(torch.argmax(pred).item())]}")
+    print(f"\nGaze is facing:\t{is_gaze_facing}\t\n\tRetrieval status: {retrieval_return_string}\t\n\tEmotion Detected: {net.predict(return_frame)}")
     cv2.imshow('frame',  return_frame)
                 
     if cv2.waitKey(1) & 0xFF == ord('q'):
